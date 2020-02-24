62678c5276ebc7cf06335ba91cf92de001be1816
Thiago Vieira <th91v@outlook.com>
2020-02-24 15:12:10 -0300

    configuração da Data Layer Access, configuração do acesso banco de dados

--- LojaBrinquedos/Controllers/HomeController.cs
+++ LojaBrinquedos/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LojaBrinquedos.Models;
+using LojaBrinquedos.Uteis;
 
 namespace LojaBrinquedos.Controllers
 {
---
+++ LojaBrinquedos/Uteis/DAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace LojaBrinquedos.Uteis
+{
+    //Data Access Layer
+    public class DAL
+    {
+        private static string Server = "localhost";
+        private static string Database = "loja_brinquedos";
+        private static string User = "root";
+        private static string Password = "";
+        private static string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=none;Charset=utf8;";
+        private static MySqlConnection Connection;
+
+        public DAL()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+
+        // Espera um parâmetro do tipo string contendo um comando SQl do tipo SELECT
+        public DataTable RetDataTable(string sql)
+        {
+            DataTable data = new DataTable();
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            MySqlDataAdapter DataAdapter = new MySqlDataAdapter(Command);
+            DataAdapter.Fill(data);
+            return data;
+        }
+
+        // Espera um parâmetro do tipo string contendo um comando SQl do tipo INSERT, UPDATE, DELETE
+        // É executado primeiramente em HomeController
+        public void ExecutarComandoSQL(string sql) 
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            Command.ExecuteNonQuery();
+        }
+    }
+}
