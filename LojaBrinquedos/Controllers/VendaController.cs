a7779f3827075b045567d30a345b0529652943d5
Thiago Vieira <th91v@outlook.com>
2020-02-26 15:46:36 -0300

    CRUD de vendas[EM PROGRESSO]

---
+++ LojaBrinquedos/Controllers/VendaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaBrinquedos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaBrinquedos.Controllers
+{
+    public class VendaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            CarregarDados();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(VendaModel venda)
+        {
+            return View();
+        }
+
+        private void CarregarDados()
+        {
+            ViewBag.ListaClientes = new VendaModel().RetornarListaClientes();
+            ViewBag.ListaVendedores = new VendaModel().RetornarListaVendedores();
+            ViewBag.ListaProdutos = new VendaModel().RetornarListaProdutos();
+        }
+    }
+}
---
+++ LojaBrinquedos/Models/VendaModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaBrinquedos.Models
+{
+    public class VendaModel
+    {
+        public string Id { get; set; }
+        public string Cliente_Id { get; set; }
+        public string Vendedor_Id { get; set; }
+
+        public List<ClienteModel> RetornarListaClientes()
+        {
+            return new ClienteModel().ListarTodosClientes(); // acessa o método de ClienteModel por referência
+        }
+
+        public List<VendedorModel> RetornarListaVendedores()
+        {
+            return new VendedorModel().ListarTodosVendedores();
+        }
+
+        public List<ProdutoModel> RetornarListaProdutos()
+        {
+            return new ProdutoModel().ListarTodosProdutos();
+        }
+    }
+}
